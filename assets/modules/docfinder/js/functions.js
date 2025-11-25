fa6018a0c67ae54effb9dc3011805850a4522ed5
yamamoto <yamamoto@kyms.jp>
2025-11-25 22:51:41 +0900

    Format DocFinder JavaScript indentation

--- assets/modules/docfinder/js/functions.js
+++ assets/modules/docfinder/js/functions.js
@@ -20,74 +20,69 @@
 //
 //
 
-
 // define global parameters
 let ajaxRequest, delay, timer, oldSearchstring;
 let time = 0;
 
-window.addEvent('load', function () {
-
+window.addEvent("load", function () {
     // check search string and set submit buttons
     triggerSubmitButtons();
 
     // init date pickers
-    new vlaDatePicker('createdon_start', {
-        style: 'apple_widget',
+    new vlaDatePicker("createdon_start", {
+        style: "apple_widget",
         offset: { x: 3, y: -1 },
-        separator: '-',
-        filePath: module_url + 'js/vlaCal.v2/inc/'
+        separator: "-",
+        filePath: module_url + "js/vlaCal.v2/inc/",
     });
 
-    new vlaDatePicker('createdon_end', {
-        style: 'apple_widget',
+    new vlaDatePicker("createdon_end", {
+        style: "apple_widget",
         offset: { x: 3, y: -1 },
-        separator: '-',
-        filePath: module_url + 'js/vlaCal.v2/inc/'
+        separator: "-",
+        filePath: module_url + "js/vlaCal.v2/inc/",
     });
 
-    new vlaDatePicker('editedon_start', {
-        style: 'apple_widget',
+    new vlaDatePicker("editedon_start", {
+        style: "apple_widget",
         offset: { x: 3, y: -1 },
-        separator: '-',
-        filePath: module_url + 'js/vlaCal.v2/inc/'
+        separator: "-",
+        filePath: module_url + "js/vlaCal.v2/inc/",
     });
 
-    new vlaDatePicker('editedon_end', {
-        style: 'apple_widget',
+    new vlaDatePicker("editedon_end", {
+        style: "apple_widget",
         offset: { x: 3, y: -1 },
-        separator: '-',
-        filePath: module_url + 'js/vlaCal.v2/inc/'
+        separator: "-",
+        filePath: module_url + "js/vlaCal.v2/inc/",
     });
-
 
     // get searchstring
-    oldSearchstring = $('searchstring').value;
-
+    oldSearchstring = $("searchstring").value;
 
     // catch submit
-    $('docfinder').addEvent('submit', function (e) {
+    $("docfinder").addEvent("submit", function (e) {
         new Event(e).stop();
-        $('replace_mode').value = 0;
+        $("replace_mode").value = 0;
         startAjaxSearch();
     });
 
     // catch submit button
-    $('submit_search').addEvent('click', function (e) {
+    $("submit_search").addEvent("click", function (e) {
         new Event(e).stop();
-        $('replace_mode').value = 0;
+        $("replace_mode").value = 0;
         startAjaxSearch();
     });
 
     // catch replace button
-    $('submit_replace').addEvent('click', function (e) {
+    $("submit_replace").addEvent("click", function (e) {
         new Event(e).stop();
-        $('replace_mode').value = 1;
+        $("replace_mode").value = 1;
         startAjaxSearch();
     });
 
-
     // cancel search and AJAX search request
-    $('cancel_search').addEvent('click', function (e) {
+    $("cancel_search").addEvent("click", function (e) {
         new Event(e).stop();
 
         // clear delay and AJAX request
@@ -95,65 +90,72 @@ window.addEvent('load', function () {
         if (ajaxRequest) ajaxRequest.cancel();
 
         // hide AJAX load inidcator
-        $('ajax_load_indicator').setStyles('display: none;');
+        $("ajax_load_indicator").setStyles("display: none;");
 
         // set text to results container and show it
-        $('results_container').setText('Search canceled.');
-        $('results_container').setStyles('display: inline;');
+        $("results_container").setText("Search canceled.");
+        $("results_container").setStyles("display: inline;");
 
         // stop timer
         $clear(timer);
         time = 0;
 
         // set results info
-        $('results_info').setText("0");
+        $("results_info").setText("0");
     });
 
     // toggle search field text
     function toggleRegExpSearch() {
-        if ($('regular_expression').getValue() == false) $('search_field_text').setText('String search:');
-        else $('search_field_text').setText('RegExp search:');
+        if ($("regular_expression").getValue() == false) $("search_field_text").setText("String search:");
+        else $("search_field_text").setText("RegExp search:");
     }
 
     // catch event for regular_expression
-    $('regular_expression').addEvent('click', function (e) {
+    $("regular_expression").addEvent("click", function (e) {
         e = new Event(e);
         toggleRegExpSearch();
     });
 
     // set search field text when page loads
-    if ($('regular_expression').getValue() == "on") toggleRegExpSearch();
-
+    if ($("regular_expression").getValue() == "on") toggleRegExpSearch();
 });
-
 
 // perform AJAX request
 const startAjaxSearch = function () {
-    if ($('searchstring').value == "") return;
+    if ($("searchstring").value == "") return;
 
     // check if this a replace
-    if ($('replace_mode').value == 1) {
+    if ($("replace_mode").value == 1) {
         let infoText;
         // abort if regular expressions are on
-        if ($('regular_expression').getValue() == "on") infoText = "The replace function can not be combinded with regular expressions.";
+        if ($("regular_expression").getValue() == "on")
+            infoText = "The replace function can not be combinded with regular expressions.";
 
         // abort if logical operators are used
-        if ($('searchstring').value.test("AND") || $('searchstring').value.test("OR") || $('searchstring').value.test("NOT")) infoText = "The replace function can not be combined with logic operators.";
+        if (
+            $("searchstring").value.test("AND") ||
+            $("searchstring").value.test("OR") ||
+            $("searchstring").value.test("NOT")
+        )
+            infoText = "The replace function can not be combined with logic operators.";
 
         // abort if the ALL operator is used
-        if ($('searchstring').value == "ALL") infoText = "The replace function can not be combined with the ALL operator.";
+        if ($("searchstring").value == "ALL")
+            infoText = "The replace function can not be combined with the ALL operator.";
 
         // alert abort info, reset replace mode var and cancel function
         if (infoText) {
             alert(infoText);
-            $('replace_mode').value = 0;
+            $("replace_mode").value = 0;
             return;
         }
 
         // Ask user for confirmation
-        const confirmation = confirm("Replace all instances of “" + $('searchstring').value + "” with “" + $('replacestring').value + "”?");
+        const confirmation = confirm(
+            "Replace all instances of “" + $("searchstring").value + "” with “" + $("replacestring").value + "”?",
+        );
         if (confirmation == false) {
-            $('replace_mode').value = 0;
+            $("replace_mode").value = 0;
             return;
         }
     }
@@ -166,169 +168,157 @@ const startAjaxSearch = function () {
     time = 0;
 
     // start timer
-    $('time_info').setStyles('display: inline;');
-    $('time').setText('0');
+    $("time_info").setStyles("display: inline;");
+    $("time").setText("0");
     timer = setTimer.periodical(100);
 
     // hide old results table
-    if (window.ie7) $('results_container').setStyles('visibility: hidden;');
-    else $('results_container').setStyles('display: none;');
+    if (window.ie7) $("results_container").setStyles("visibility: hidden;");
+    else $("results_container").setStyles("display: none;");
 
     // show AJAX load inidcator
-    $('ajax_load_indicator').setStyles('display: block;');
+    $("ajax_load_indicator").setStyles("display: block;");
 
     // set recognition for ajax mode
-    $('checkform').value = 'ajax_get_results';
+    $("checkform").value = "ajax_get_results";
 
     // set update session recognition
-    $('update_session').value = 1;
+    $("update_session").value = 1;
 
     // get search string
-    const searchstring = $('searchstring').getValue();
+    const searchstring = $("searchstring").getValue();
 
     // set results string
-    if (searchstring) $('search_string').setText(searchstring);
+    if (searchstring) $("search_string").setText(searchstring);
 
     // send form and update results
     // Ensure CSRF token is included in the form
-    if (typeof csrf_token !== 'undefined') {
-        $('csrf_token').value = csrf_token;
+    if (typeof csrf_token !== "undefined") {
+        $("csrf_token").value = csrf_token;
     }
-    ajaxRequest = $('docfinder').send({
-        update: $('results_container'),
+    ajaxRequest = $("docfinder").send({
+        update: $("results_container"),
         autoCancel: true,
         evalScripts: true,
         onComplete: function () {
             // hide AJAX load inidcator
-            $('ajax_load_indicator').setStyles('display: none;');
+            $("ajax_load_indicator").setStyles("display: none;");
 
             // show results section and table
-            $('results_section').setStyles('display: block;');
-            $('results_container').setStyles('display: block;');
+            $("results_section").setStyles("display: block;");
+            $("results_container").setStyles("display: block;");
 
             // update histories
             updateSearchHistory("search");
-            if ($('replace_mode').value == 1) updateSearchHistory("replace");
+            if ($("replace_mode").value == 1) updateSearchHistory("replace");
 
             // clear reset replace mode
-            if ($('replace_mode').value) $('replace_mode').value = 0;
+            if ($("replace_mode").value) $("replace_mode").value = 0;
 
             ajaxRequest = false;
         },
-        onCancel: function () {
-
-        }
+        onCancel: function () {},
     });
-}
-
+};
 
 function updateSearchHistory(type) {
     // set recognition for AJAX mode
     let querystring = "checkform=ajax_get_" + type + "History";
 
     // add CSRF token to request
-    if (typeof csrf_token !== 'undefined') {
+    if (typeof csrf_token !== "undefined") {
         querystring += "&csrf_token=" + encodeURIComponent(csrf_token);
     }
 
     // get update via AJAX
     const historyAjaxUpdate = new Ajax(window.location.href, {
-        method: 'post',
-        update: $(type + '_history_box'),
+        method: "post",
+        update: $(type + "_history_box"),
         autoCancel: true,
         evalScripts: false,
-        onComplete: function () {
-
-        },
-        onCancel: function () {
-
-        }
+        onComplete: function () {},
+        onCancel: function () {},
     }).request(querystring);
 }
-
 
 // refresh time function
 const setTimer = function () {
     if (ajaxRequest) {
         // increment timer
         time++;
-        $('time').setText(time / 10);
-    }
-    else {
+        $("time").setText(time / 10);
+    } else {
         // stop timer
         $clear(timer);
         time = 0;
     }
-}
+};
 
 function triggerSubmitButtons() {
     // set or remove active class depending if if the search string is empty
-    if ($('searchstring').value != "") $('submit').addClass('submit_active').removeClass('submit_passive');
-    else $('submit').addClass('submit_passive').removeClass('submit_active');
+    if ($("searchstring").value != "") $("submit").addClass("submit_active").removeClass("submit_passive");
+    else $("submit").addClass("submit_passive").removeClass("submit_active");
 }
 
 function checkboxSelector(id) {
     // Search places
     if (id == "search_place_selector") {
         if ($(id).checked) {
-            $('search_place_DocAndTVV').checked = true;
-            $('search_place_Templates').checked = true;
-            $('search_place_TVs').checked = true;
-            $('search_place_Chunks').checked = true;
-            $('search_place_Snippets').checked = true;
-            $('search_place_Plugins').checked = true;
-            $('search_place_Modules').checked = true;
-        }
-        else {
-            $('search_place_DocAndTVV').checked = false;
-            $('search_place_Templates').checked = false;
-            $('search_place_TVs').checked = false;
-            $('search_place_Chunks').checked = false;
-            $('search_place_Snippets').checked = false;
-            $('search_place_Plugins').checked = false;
-            $('search_place_Modules').checked = false;
+            $("search_place_DocAndTVV").checked = true;
+            $("search_place_Templates").checked = true;
+            $("search_place_TVs").checked = true;
+            $("search_place_Chunks").checked = true;
+            $("search_place_Snippets").checked = true;
+            $("search_place_Plugins").checked = true;
+            $("search_place_Modules").checked = true;
+        } else {
+            $("search_place_DocAndTVV").checked = false;
+            $("search_place_Templates").checked = false;
+            $("search_place_TVs").checked = false;
+            $("search_place_Chunks").checked = false;
+            $("search_place_Snippets").checked = false;
+            $("search_place_Plugins").checked = false;
+            $("search_place_Modules").checked = false;
         }
     }
 
     // Documents search in
     if (id == "documents_search_in_selector") {
         if ($(id).checked) {
-            $('df_id').checked = true;
-            $('pagetitle').checked = true;
-            $('longtitle').checked = true;
-            $('description').checked = true;
-            $('alias').checked = true;
-            $('introtext').checked = true;
-            $('menutitle').checked = true;
-            $('content').checked = true;
-            $('tvs').checked = true;
-        }
-        else {
-            $('df_id').checked = false;
-            $('pagetitle').checked = false;
-            $('longtitle').checked = false;
-            $('description').checked = false;
-            $('alias').checked = false;
-            $('introtext').checked = false;
-            $('menutitle').checked = false;
-            $('content').checked = false;
-            $('tvs').checked = false;
+            $("df_id").checked = true;
+            $("pagetitle").checked = true;
+            $("longtitle").checked = true;
+            $("description").checked = true;
+            $("alias").checked = true;
+            $("introtext").checked = true;
+            $("menutitle").checked = true;
+            $("content").checked = true;
+            $("tvs").checked = true;
+        } else {
+            $("df_id").checked = false;
+            $("pagetitle").checked = false;
+            $("longtitle").checked = false;
+            $("description").checked = false;
+            $("alias").checked = false;
+            $("introtext").checked = false;
+            $("menutitle").checked = false;
+            $("content").checked = false;
+            $("tvs").checked = false;
         }
     }
 
     // Resources search in
     if (id == "resources_search_in_selector") {
         if ($(id).checked) {
-            $('resources_id').checked = true;
-            $('resources_name').checked = true;
-            $('resources_description').checked = true;
-            $('resources_other').checked = true;
-        }
-        else {
-            $('resources_id').checked = false;
-            $('resources_name').checked = false;
-            $('resources_description').checked = false;
-            $('resources_other').checked = false;
+            $("resources_id").checked = true;
+            $("resources_name").checked = true;
+            $("resources_description").checked = true;
+            $("resources_other").checked = true;
+        } else {
+            $("resources_id").checked = false;
+            $("resources_name").checked = false;
+            $("resources_description").checked = false;
+            $("resources_other").checked = false;
         }
     }
 }
